530aadd2e90eb51b78854ab09757d4ef37b2ea31
WilliamSegrestAU <wbs0026@auburn.edu>
2022-09-22 13:14:18 -0500

    Script can determine winner and play a round
    The playRound() function asks for user input and prints out the result of the game to the console.
    
    To determine the winner, the deterWinner function uses a switch function based on the users input. It returns a string indicating if the player won or not. If the user input was not correct, it prompts the user that they did not put in a valid input.

--- game.js
+++ game.js
@@ -1,23 +1,74 @@
 const choice = ['Rock', 'Paper', 'Scissors'];
 
 
-// Function will play as the CPU.
-// Returns 'Rock', 'Paper', 'Scissor'
+// Plays as the CPU.
+// Returns 'Rock', 'Paper', 'Scissors'
 function getComputerChoice() {
     const rand = Math.random();
     let value = rand * choice.length;
     let index = Math.floor(value);
 
-    console.log(choice[index]);
     return choice[index];
 }
 
-// Function will ask for user input
+// Asks for user input
 function getPlayerChoice() {
     let choice = prompt("Rock, Paper, or Scissors?");
-    choice = choice.Title();
 
     return choice;
 }
 
+// Serves as a round played of 'Rock', 'Paper', 'Scissors'
+function playRound() {
+    let Player = getPlayerChoice();
+    let CPU = getComputerChoice();
+
+    let result = determineWinner(Player, CPU);
+    console.log(result);
+}
+
+// Based on user input and CPU choice, function will determine round winner
+function determineWinner(player, cpu) {
+    switch (player.toUpperCase()) {
+        // Player chose Rock
+        case 'ROCK':
+            if (cpu == 'Paper') {
+                return "You Lose! Paper beats Rock";
+            }
+            else if (cpu == 'Scissors') {
+                return "You Win! Rock beats Scissors";
+            }
+            else {
+                return "You Tied! You both chose Rock";
+            }
+        
+        // Player chose Paper
+        case 'PAPER':
+            if (cpu == 'Rock') {
+                return "You Win! Paper beats Rock";
+            }
+            else if (cpu == 'Scissors') {
+                return "You Lose! Scissors beat Paper";
+            }
+            else {
+                return "You Tied! You both chose Paper";
+            }
+
+        // Player choose Scissors
+        case 'SCISSORS':
+            if (cpu == 'Rock') {
+                return "You Lose! Rock beats Scissors";
+            }
+            else if (cpu == 'Paper') {
+                return "You Win! Scissors beat Paper";
+            }
+            else {
+                return "You Tied! You both chose Scissors";
+            }
+
+        // User input is not "Rock", "Paper", or "Scissors"
+       default:
+           return "You didn't choose Rock, Paper, or Scissors!" 
+    }
+}
 
